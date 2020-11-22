8b60196d55519ea1ce7e9bfee236c614bf89fe9c
Becky Robran <rerobran@gmail.com>
2020-11-22 08:31:02 -0700

    adds sign in and logout to controller

--- controllers/users.js
+++ controllers/users.js
@@ -2,33 +2,67 @@ const User = require('../models/user');
 var db = require('../models');
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
-exports.postCreateUser = (req, res, next) => {
-    const user = new User(req.body.first_name, req.body.last_name, req.body.password, req.body.user_name, req.body.email);
-    user.save();
-    res.redirect('/');
-  };
 
   exports.homepage = (req, res, next) => {
-    res.send("Hello world")
+    res.render('homepage')
   };
 
-
- exports.createUser = (req, res, next) => {
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-      db.User.create({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      userName: req.body.user_name,
-      email: req.body.email,
-      password: hash
-      }).then(function(data) {
-        if (data) {
-        req.session.user = data;
-        res.redirect('/');
+  exports.signInUser = (req, res, next) => {
+    db.User.findOne({
+      where: {
+          email: req.body.email
+             }
+      })
+      .then((user) => {
+        if(user && bcrypt.compareSync(req.body.password, user.password)) {
+          req.session.user = user;
+          console.log("@!!!!!, session", req.session.user)
+          res.redirect('/')
+        }
+        else {
+          res.render('signin', {message: "Invalid credentials!"});
         }
       })
-      .catch((error) => {
-        console.log(`Error ${error}`)
-      });
-   });
+  };
+
+  exports.createUser = (req, res, next) => {
+    if(!req.body.user_name || !req.body.password){
+        res.status("400");
+        res.send("Invalid details!");
+     } 
+     db.User.findOne({
+      where: {
+          email: req.body.email
+             }
+      })
+      .then(function (user) {
+        if (user) {
+          res.render('signup', {
+            message: "User Already Exists! Login or choose another user id"});
+        } 
+        else {
+          bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+            db.User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            userName: req.body.user_name,
+            email: req.body.email,
+            password: hash
+            }).then(function(data) {
+              if (data) {
+              req.session.user = data;
+              res.redirect('/');
+              }
+            })
+            .catch((error) => {
+              console.log(`Error ${error}`)
+            });
+          });
+        }
+      })
+  };
+
+  exports.logout = (req, res, next) => {
+    req.session.destroy
+    res.render('homepage', {message: "You are logged out."})
   };
