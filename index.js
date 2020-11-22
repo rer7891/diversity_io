ca3b4c9becf2fe8d081240a5dd79392486076d4f
Becky Robran <rerobran@gmail.com>
2020-11-21 20:51:43 -0700

    updates index wiht routes and views

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.json());
-app.get('/', (req, res) => res.json({message: 'Hello World'}))
-app.listen(port, ()=> console.log(`Your app is running on ${port}`))
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(cookieParser());
+app.use(session({secret: "Your secret key"}));
+
+const routes = require('./routes/users');
+
+app.use('/', routes);
+
+app.listen(3000);
