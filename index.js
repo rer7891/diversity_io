caff56c44ec6ca357f81bbc04b35692b3da9b444
Becky Robran <rerobran@gmail.com>
2020-11-23 11:40:04 -0700

    adds dotenv package

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var cookieParser = require('cookie-parser');
-
+var dev = require('dotenv').config()
 
 app.set('view engine', 'pug');
 app.set('views','./views');
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(session({secret: "Your secret key"}));
 
-const routes = require('./routes/users');
+const userRoutes = require('./routes/users');
+const postRoutes = require('./routes/posts');
 
-app.use('/', routes);
+app.use(userRoutes);
+app.use(postRoutes);
 
 app.listen(3000);
